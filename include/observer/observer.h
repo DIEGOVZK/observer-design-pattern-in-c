1afcea70beabee512ba1177bc818e8b212b1b3e2
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2023-11-21 09:28:05 -0300

    Add Observer interface and ConcreteObserver implementation

---
+++ include/observer/observer.h
@@ -0,0 +1,17 @@
+/**
+ * @brief Observer Interface
+ * @param update Function to update the Observer with an event. Takes the event as a string.
+ */
+typedef struct
+{
+    void (*update)(const char *event);
+} Observer;
+
+/**
+ * @brief Concrete implementation of the Observer interface.
+ * @param base The base Observer interface this struct is built on.
+ */
+typedef struct
+{
+    Observer base;
+} ConcreteObserver;

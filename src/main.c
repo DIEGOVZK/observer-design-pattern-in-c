8bceebf2b043c3d1db71728c143bfdb7e60cf9ed
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2023-11-21 09:15:51 -0300

    Update .gitignore and main.c

--- src/main.c
+++ src/main.c
@@ -2,19 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Observer Interface
-typedef struct {
-    void (*update)(const char* event);
-} Observer;
-
-// Subject (Observable) Interface
-typedef struct {
-    void (*attach)(Observer* observer);
-    void (*detach)(Observer* observer);
-    void (*notify)(const char* event);
-} Subject;
-
-int main ()
+int main()
 {
-    
+
+    return 0;
 }

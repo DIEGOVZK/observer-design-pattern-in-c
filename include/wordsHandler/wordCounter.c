7f505307bf1b4c9700f8d587d7cc01a9626ac065
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2023-11-21 10:27:10 -0300

    Add word counting functions and update documentation

---
+++ include/wordsHandler/wordCounter.c
@@ -0,0 +1,97 @@
+#include "wordCounter.h"
+
+void countWords(Subject *subject, const char *sentence)
+{
+    int totalWords = countTotalWords(sentence);
+    int evenLengthWords = countEvenLengthWords(sentence);
+    int capitalStartWords = countCapitalStartWords(sentence);
+
+    // Notify observers
+    char event[100];
+    sprintf(event, "Total words: %d, Even length words: %d, Capital start words: %d",
+            totalWords, evenLengthWords, capitalStartWords);
+    notify(subject, event);
+}
+
+int countTotalWords(const char *sentence)
+{
+    int totalWords = 0;
+    int isWord = 0;
+    for (const char *ptr = sentence; *ptr; ++ptr)
+    {
+        if (isalpha((unsigned char)*ptr))
+        {
+            isWord = 1;
+        }
+        else
+        {
+            if (isWord)
+            {
+                totalWords++;
+                isWord = 0;
+            }
+        }
+    }
+    if (isWord)
+    {
+        totalWords++;
+    }
+    return totalWords;
+}
+
+int countEvenLengthWords(const char *sentence)
+{
+    int evenLengthWords = 0;
+    int wordLength = 0;
+    int isWord = 0;
+    for (const char *ptr = sentence; *ptr; ++ptr)
+    {
+        if (isalpha((unsigned char)*ptr))
+        {
+            wordLength++;
+            isWord = 1;
+        }
+        else
+        {
+            if (isWord)
+            {
+                if (wordLength % 2 == 0)
+                {
+                    evenLengthWords++;
+                }
+                wordLength = 0;
+                isWord = 0;
+            }
+        }
+    }
+    if (isWord && wordLength % 2 == 0)
+    {
+        evenLengthWords++;
+    }
+    return evenLengthWords;
+}
+
+int countCapitalStartWords(const char *sentence)
+{
+    int capitalStartWords = 0;
+    int wordLength = 0;
+    for (const char *ptr = sentence; *ptr; ++ptr)
+    {
+        if (isalpha((unsigned char)*ptr))
+        {
+            wordLength++;
+            if (wordLength == 1)
+            {
+                if (isupper((unsigned char)*ptr))
+                {
+                    capitalStartWords++;
+                }
+            }
+        }
+        else
+        {
+            wordLength = 0;
+        }
+    }
+    return capitalStartWords;
+}
--- include/wordsHandler/wordCounter.h
+++ include/wordsHandler/wordCounter.h
@@ -4,6 +4,27 @@
 #include "observer.h"
 
 /**
+ * @brief Counts the number of words in a sentence.
+ * @param sentence The sentence in which to count the words.
+ * @return The number of words in the sentence.
+ */
+int countTotalWords(const char *sentence);
+
+/**
+ * @brief Counts the number of words in a sentence that have an even length.
+ * @param sentence The sentence in which to count the words.
+ * @return The number of words in the sentence that have an even length.
+ */
+int countEvenLengthWords(const char *sentence);
+
+/**
+ * @brief Counts the number of words in a sentence that start with a capital letter.
+ * @param sentence The sentence in which to count the words.
+ * @return The number of words in the sentence that start with a capital letter.
+ */
+int countCapitalStartWords(const char *sentence);
+
+/**
  * @brief Counts the number of words in a sentence and notifies the observers.
  * @param subject The subject that will notify the observers.
  * @param sentence The sentence in which to count the words.

44a0824ae738dfc358ccbed2ddf33b060908c735
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2023-11-21 09:07:37 -0300

    Refactor main.c to implement Observer pattern

--- src/main.c
+++ src/main.c
@@ -1,4 +1,18 @@
-#include <unity.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Observer Interface
+typedef struct {
+    void (*update)(const char* event);
+} Observer;
+
+// Subject (Observable) Interface
+typedef struct {
+    void (*attach)(Observer* observer);
+    void (*detach)(Observer* observer);
+    void (*notify)(const char* event);
+} Subject;
 
 int main ()
 {
